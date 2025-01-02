10b17d79ea4c1d300c3e66baf422bef7f36be54a
Gowthams1999-commits <githubgowtham1999@gmail.com>
2025-01-02 23:49:34 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 set -e
 
-# Stop the running container (if any)
-echo "Hi"
+# Define the container name (make sure to set this to the correct container name)
+CONTAINER_NAME=myapp-container
+
+# Stop the running container if it exists
+if [ $(docker ps -q -f name=$CONTAINER_NAME) ]; then
+    echo "Stopping container: $CONTAINER_NAME"
+    docker stop $CONTAINER_NAME
+else
+    echo "No running container found for $CONTAINER_NAME"
+fi
