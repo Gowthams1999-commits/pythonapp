82c4e78e6bf1fd89794cb6219dd95536b164d474
Gowthams1999-commits <githubgowtham1999@gmail.com>
2025-01-03 00:46:19 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 set -e
 
+DOCKER_NAME="py-test"
 # Define the Docker image name (make sure to set this to the correct image name)
 DOCKER_IMAGE="gowtham904/simple-pythonapp:latest"
 
+docker rm -f $DOCKER_NAME
 # Pull the Docker image from Docker Hub (if needed)
 echo "Pulling Docker image: $DOCKER_IMAGE"
 docker pull $DOCKER_IMAGE
 
 # Run the Docker container
 echo "Starting container: $DOCKER_IMAGE"
-docker run -d --name py-test -p 80:80 $DOCKER_IMAGE
+docker run -d --name $DOCKER_NAME -p 80:80 $DOCKER_IMAGE
